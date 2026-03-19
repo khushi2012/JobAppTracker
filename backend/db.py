805144c0cfd5450fc55991f435a737c06c745832
Khushi Sinha <reachkhushihere@gmail.com>
2026-03-19 18:37:06 -0400

    hosted backend on supabase

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,14 @@
-from sqlalchemy import create_engine # type: ignore
-from sqlalchemy.orm import sessionmaker, declarative_base # type: ignore
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./jobs.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
