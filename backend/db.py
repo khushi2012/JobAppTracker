08fcc95a3ee857c684b9b00d9ab749691a4016ad
Khushi Sinha <reachkhushihere@gmail.com>
2026-03-21 19:01:14 -0400

    fixed db and fixed google import version

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
